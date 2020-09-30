bc849a5e75aa0d6c04f80dff22aec0eac7fe3a7f
Ray Feng <ryfprivate@outlook.com>
2020-10-01 01:42:18 +1000

    Removed unused controllers/helpers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 export * from './cognito.controller';
 export * from './file-storage.controller';
 export * from './ping.controller';
-export * from './test.controller';
-
 export * from './portfolios.controller';
 export * from './users.controller';
+
+
