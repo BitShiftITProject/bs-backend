2a23daba6ce0f099fef18cf7d2f48f3fd758def0
Dini <mahardinip@student.unimelb.edu.au>
2020-09-30 12:01:38 +1000

    Added string Title property to Page model

--- src/models/pages.model.ts
+++ src/models/pages.model.ts
@@ -2,13 +2,18 @@ import {Entity, model, property} from '@loopback/repository';
 
 @model()
 export class Pages extends Entity {
-
   @property({
     type: 'string',
     id: true,
     generated: true,
   })
   id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
 
   @property({
     type: 'object',
