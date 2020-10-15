8233d7a95346ed7f3d244af05e0f8a4b1fc4bf2e
namanj123in <36196385+namanj123in@users.noreply.github.com>
2020-10-16 01:13:37 +1100

    Add MediaItems controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -8,3 +8,4 @@ export * from './users.controller';
 export * from './users-portfolios.controller';
 export * from './portfolios-pages.controller';
 export * from './pages.controller';
+export * from './media-items.controller';
